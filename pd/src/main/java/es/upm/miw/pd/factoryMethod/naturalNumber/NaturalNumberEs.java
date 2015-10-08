4be754b7c41930deea27b0d5470346e0256343a5
África <africa_93@hotmail.com>
2015-10-08 13:57:25 +0200

    commit

--- pd/src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEs.java
+++ pd/src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEs.java
@@ -1,33 +1,30 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber;
 
-public class NaturalNumberEs {
-    private int value;
+public class NaturalNumberEs extends Natu {
+	private int value;
 
-    private static final String[] textValue = {"cero", "uno", "dos", "tres", "cuatro", "cinco"};
+	private static final String[] textValue = { "cero", "uno", "dos", "tres", "cuatro", "cinco" };
 
-    public NaturalNumberEs(int value) {
-        this.setValue(value);
-    }
+	public NaturalNumberEs(int value) {
+		this.setValue(value);
+	}
 
-    public int getValue() {
-        return value;
-    }
 
-    public void setValue(int value) {
-        assert value >= 0;
-        this.value = value;
-    }
+	public void setValue(int value) {
+		assert value >= 0;
+		this.value = value;
+	}
 
-    public void add(int value) {
-        this.setValue(this.value + value);
-    }
+	public void add(int value) {
+		this.setValue(this.value + value);
+	}
 
-    public String getTextValue() {
-        if (this.value < textValue.length) {
-            return NaturalNumberEs.textValue[this.value];
-        } else {
-            return "???";
-        }
-    }
+	public String getTextValue() {
+		if (this.value < textValue.length) {
+			return NaturalNumberEs.textValue[this.value];
+		} else {
+			return "???";
+		}
+	}
 
 }
